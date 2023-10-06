9492b4283306371d3f8d1cb90aa8add52b3d651f
Zalan Demeter <zalandemeter12@gmail.com>
2023-10-06 19:20:52 -0400

    first layout works

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # robonaut-telemetry
+
+## Building from source
+
+### Fetch source and install dependencies
+
+```bash
+cd <path/to/your/ros_ws>
+git clone https://github.com/foxglove/ros-foxglove-bridge.git src/ros-foxglove-bridge
+rosdep update
+rosdep install --ignore-src --default-yes --from-path src
+```
+
+### ROS 2
+```
+colcon build --event-handlers console_direct+ --symlink-install
+source install/local_setup.bash
+ros2 launch foxglove_bridge foxglove_bridge_launch.xml
+```
--- src/telemetry.cc
+++ src/telemetry.cc
@@ -1,20 +1,56 @@
 #include <cstdio>
-#include <rclcpp/rclcpp.hpp>
 #include <memory>
 
-class Telemetry : public rclcpp::Node
+#include "JLB/udp.hxx"
+#include "JLB/common.hxx"
+#include "JLB/signals.hxx"
+
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/float32.hpp>
+
+namespace jlb
 {
-public:
-  Telemetry() : Node("telemetry")
+  class Telemetry : public rclcpp::Node
   {
-    RCLCPP_INFO(this->get_logger(), "Hello, world!");
-  }
-};
+  public:
+    Telemetry() : Node("telemetry"), server(SERVER_ADDRESS, SERVER_PORT)
+    {
+      RCLCPP_INFO(this->get_logger(), "node started.");
+
+      for (const auto &signal : jlb::SignalLibrary::get_instance().signals)
+      {
+        publishers.push_back(this->create_publisher<std_msgs::msg::Float32>(signal.name, 10));
+      }
+
+      while (rclcpp::ok())
+      {
+        char msg[5] = {0};
+
+        if (server.recv(msg, sizeof(msg)) > 0)
+        {
+          jlb::Value value{msg, sizeof(msg)};
+          auto msg = std_msgs::msg::Float32();
+          msg.data = value.value;
+          publishers.at(value.id)->publish(msg);
+        }
+        rclcpp::spin_some(this->get_node_base_interface());
+      }
+    }
+    ~Telemetry()
+    {
+      RCLCPP_INFO(this->get_logger(), "node stopped.");
+    }
+
+  private:
+    UDPServer server;
+    std::vector<rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr> publishers;
+  };
+}
 
 int main(int argc, char **argv)
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<Telemetry>());
+  rclcpp::spin(std::make_shared<jlb::Telemetry>());
   rclcpp::shutdown();
   return EXIT_SUCCESS;
 }
